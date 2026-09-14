cb8740f016bf7399b8adebdb643a384679d97781
Alexey Yuzhakov <sibprogrammer@gmail.com>
2026-09-14 20:47:40 +0100

    Update to v0.0.5

--- Formula/psync.rb
+++ Formula/psync.rb
@@ -1,16 +1,16 @@
 class Psync < Formula
   desc "A utility to sync source code with a remote machine"
   homepage "https://github.com/plesk/psync"
-  version "0.0.4"
+  version "0.0.5"
 
   on_macos do
     on_arm do
-      url "https://github.com/plesk/psync/releases/download/v0.0.4/psync_0.0.4_darwin_arm64.tar.gz"
-      sha256 "541e16f95d263edb17d3f8d417fa9b65707de5c9dfa79c66f6e18363af756dcd"
+      url "https://github.com/plesk/psync/releases/download/v0.0.5/psync_0.0.5_darwin_arm64.tar.gz"
+      sha256 "91103c2c59f2d4dda0830acebb88633169ab8204ec192c73dc7cbabfd14547f1"
     end
     on_intel do
-      url "https://github.com/plesk/psync/releases/download/v0.0.4/psync_0.0.4_darwin_amd64.tar.gz"
-      sha256 "74737715db7e3d922558342cb4326d93ade8cdb864a5ea97c9fb5f92478dbf94"
+      url "https://github.com/plesk/psync/releases/download/v0.0.5/psync_0.0.5_darwin_amd64.tar.gz"
+      sha256 "dc68fb94ada03d709f11ada88e551e90f645c3b7fc01d766f60f7e6a9e28e3d4"
     end
   end
 
